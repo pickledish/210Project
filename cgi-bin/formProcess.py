9021b218b79c890d8ca7416f8e1e75b372fa8f83
Brandon Willett <bcwillett1@gmail.com>
2016-10-02 17:09:26 -0400

    Added basic Python database logic

--- cgi-bin/formProcess.py
+++ cgi-bin/formProcess.py
@@ -1,3 +1,36 @@
 #!/usr/bin python
 
-print(hi)
+import cgitb
+import cgi
+import sqlite3
+
+cgitb.enable()
+
+conn = sqlite3.connect('Users.db')
+c = conn.cursor()
+
+loginForm = cgi.FieldStorage()
+
+if ('username' in form and 'password' in form):
+	user = form['username']
+	pword = form['password']
+
+	c.execute("SELECT FROM Users * WHERE username=?", user)
+	all_rows = c.fetchall()
+
+	if (!all_rows):
+		c.execute("INSERT INTO Users VALUES (?, ?)", user, pword)
+		print 'Content-Type: text/html'
+		print
+		print '''<html>
+		    <head>
+		        <title>Your account has been created!</title>
+		    </head>
+		    <body>
+		        User 
+		    </body>
+		</html>'''
+
+
+	else:
+		print ("You already had an account dumbass")
